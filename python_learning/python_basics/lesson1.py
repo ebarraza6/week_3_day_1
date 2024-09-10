e7b4f71cbb624d65a33a805f4cba6fea56a8bf39
Barraza <ebarraza6@cps.k12.il.us>
2024-09-10 14:32:20 -0500

    first commit

---
+++ python_learning/python_basics/lesson1.py
@@ -0,0 +1,25 @@
+#mkdir means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means creat a file
+#rm means remove a file
+# pound sign means comment
+# print("Hello, World!")
+# print("Christmas")
+# print("age \t 25")
+# \t means tab
+# print("address")
+# press control ? to comment out multiple lines
+# ask for input
+name = input("What is your name? ")
+print("Hello, " + name)
+age = input("What is your age?")
+print("You are " + age + " years old")
+address = input("Where do you live?")
+print("You live in " + address)
+school= input("Where do you go to school?")
+print("You go to school at " + school )
+place_of_birth = input("you were born in " + " place of birth")
+print("You were born in " + place_of_birth )
+print("Hello, " + name + ". You are " + age + " years old. You were born in " + place_of_birth)
